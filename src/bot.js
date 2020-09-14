d8a560baec4e25adfe56e364ec03936ad3b8d9cb
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-09-14 18:42:17 +0100

    basic events

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 
-const {Client} = require('discord.js')
-const client  = new Client();
+const { Client } = require('discord.js')
+const client = new Client();
+
+// listen to event
+client.on('ready', () => {
+    console.log(`${client.user.username}`, 'has logged in',);
+})
 
 client.login(process.env.DISCODE_JS_BOT_TOKEN)
